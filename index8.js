c27a1a7938e1ba72f89623506f8e45b6d9f9ff19
vishal1238 <webervishal@gmail.com>
2026-02-20 11:53:15 +0530

    index7 updated

--- index8.js
+++ index8.js
@@ -3,12 +3,19 @@ const app = express();
 app.listen(8080, () => {
   console.log("Server Started....");
 });
-app.use(express.json())
+
+// const greet = (req,res,next) => {
+//   console.log("Hello World");
+//   next()
+  
+// };
+// app.use(greet)
+
+app.use(express.json());
+
 let users = [
   { id: 1, name: "avi", email: "avi@mail.com", role: "Student" },
-
   { id: 2, name: "satyam", email: "satyam@mail.com", role: "admin" },
-
   { id: 3, name: "vivaan", email: "vivaan@mail.com", role: "Student" },
 ];
 
@@ -17,15 +24,13 @@ app.get("/", (request, response) => {
 });
 
 app.post("/", (request, response) => {
-//   console.log(request.body);
-  const user = request.body
-  users.push(user)
+  //   console.log(request.body);
+  const user = request.body.name;
+  users.push(user);
   response.json(users);
 });
 
-app.delete("/:id",(request,response)=>{
-
-})
+app.delete("/:id", (request, response) => {});
 
 app.get("/:id", (request, response) => {
   const user = users.find((user) => user.id === Number(request.params.id));
