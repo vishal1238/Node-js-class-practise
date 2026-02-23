3f7d797d79cec0082401454f23a6d4b39c8d29a5
vishal1238 <webervishal@gmail.com>
2026-02-23 11:34:08 +0530

    files updated

---
+++ models/userModel.js
@@ -0,0 +1,8 @@
+const users = [
+    {id: 1, name: "avi", email: "avi@gmail.com", role: "Student"},
+    {id: 2, name: "praveen", email: "praveen@gmail.com", role: "Teacher"},
+    {id: 3, name: "john", email: "john@gmail.com", role: "HOD"},
+    {id: 4, name: "clark", email: "clark@gmail.com", role: "Student"}
+]
+
+export default users;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,13 @@
 import express from "express";
+import {users} from "../models/userModel.js";
 import { createUser, getUsers } from "../controllers/userController.js";
 
 const userRouter = express.Router();
+const getUsers = (req, res) => {
+    res.json(users);
+};
 
-userRouter.get("/", getUsers);
+// userRouter.get("/", getUsers);
 userRouter.post("/", createUser);
 
 export default userRouter;
