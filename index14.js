a568f5bbd368afb2941dcae4d116b249663d92a7
vishal1238 <webervishal@gmail.com>
2026-02-23 13:37:35 +0530

    index14 updated

--- index14.js
+++ index14.js
@@ -1,6 +1,16 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+const SECRET = 'lpu'
 const app = express()
 
+const user = {
+    name: "john",
+    email: "john@gmail.com",
+    role: "student"
+}
+
+const token = jwt.sign(user, SECRET,{expiresIn: "1h"})
+console.log(token);
 
 
 
