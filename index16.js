87cf2115af0dc72160a2b7587ce942ea5b444de4
vishal1238 <webervishal@gmail.com>
2026-02-24 10:29:42 +0530

    index16 updated

--- index16.js
+++ index16.js
@@ -1,38 +1,12 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { middleware,authorize } from "./middleware/auth.js";
 const secretKey = "mysecretkey";
-
 const app = express();
 app.listen(8080, () => {
   console.log("server is starting");
 });
-
-const middleware = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader)
-    return res.status(401).json({ message: "Authorization header missing" });
-  const token = authHeader.split(" ")[1];
-  try {
-    const user = jwt.verify(token, secretKey);
-    console.log("middleware executed");
-    req.user = user;
-    next();
-  } catch (err) {
-    res.status(401).json({ message: "Invalid token" });
-  }
-};
-
-const authorize = (...roles) => {
-  return (req, res, next) => {
-    if (roles.includes(req.user.role)) {
-      next();
-    } else {
-      res.status(403).json({ message: "Access denied" });
-    }
-  };
-};
-
 app.use(express.json());
 const users = [];
 app.post("/signup", (req, res) => {
