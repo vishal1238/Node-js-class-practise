0eb28b240d8f28af449036e2fc8bace962363d8c
vishal1238 <webervishal@gmail.com>
2026-02-26 11:54:05 +0530

    files updated

--- NODE-EJS/index.js
+++ NODE-EJS/index.js
@@ -1,30 +1,37 @@
 import express from "express";
 import mongoose from "mongoose";
+
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+
 const dbConnect = async () => {
   await mongoose.connect("mongodb://localhost:27017/merndatabase");
 };
+
 const startServer = async () => {
   await dbConnect();
   app.listen(8080, () => console.log("Server started"));
 };
+
 const productSchema = mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
   imageurl: { type: String, required: true },
 });
+
 const productModel = mongoose.model("products", productSchema);
 app.get("/", async (req, res) => {
   const products = await productModel.find();
   res.json(products);
 });
+
 app.post("/", async (req, res) => {
   const body = req.body;
   const result = await productModel.create(body);
   res.json({ message: "Product created" });
 });
+
 startServer();
